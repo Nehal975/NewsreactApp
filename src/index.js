de209602e5302dd6d48c5980b6d32276e29d13c4
Nehal975 <142813014+Nehal975@users.noreply.github.com>
2025-03-03 00:13:29 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-// import App from './ClassComponent/App'
+ import App from './ClassComponent/App'
 //import App from "./FunctionalComponant/App";
-import App from "./FunctionalComponant2/App";
+//import App from "./FunctionalComponant2/App";
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-    <App/>
-    // <App/>
-//    <App/>
+    
+    
+  <App/>
 )
